64ca4fe1bc1fe5efa11691aad8254430168d0e3d
bayo <bayokrapht@gmail.com>
2018-07-29 11:32:37 +0100

    allowed hosts

--- recycler/settings.py
+++ recycler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tp(8__i2jz=86jr@$)_j^u8q##b5v@ty*3%jr6gnhm=yfx#_&%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['4710d1ef.ngrok.io', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['4710d1ef.ngrok.io', 'localhost', '127.0.0.1', 'recycler.everyday.com.ng']
 
 
 # Application definition
